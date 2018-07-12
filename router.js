e92379a3c938d538811a0bac71d6180f82b7b95c
张广洲 <zhgzhll@foxmail.com>
2018-07-12 16:08:01 +0800

    login&register UI
    登陆、注册时，页面有交互

--- router.js
+++ router.js
@@ -7,24 +7,65 @@ var router = express.Router();
 
 localStorage = new LocalStorage('./scratch');
 
+var register_msg = '';
+var login_msg = '';
+
 // 创建 application/x-www-form-urlencoded 编码解析
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-//静态资源
-router.use(express.static('assests'));
-
-
 //register 注册
 router.get('/register', function(req, res) {
-    res.sendfile(__dirname + "/views/" + "register.html");
+    if (register_msg === 'user already exist') {
+        res.render("register", {
+            msg: register_msg
+        });
+    } else if (register_msg === 'passwd not same') {
+        res.render("register", {
+            msg: register_msg
+        });
+    } else {
+        register_msg = 'welcome register!';
+        res.render("register", {
+            msg: register_msg
+        });
+    }
+
 })
 
 //login登陆
 router.get('/', function(req, res) {
-    res.sendFile(__dirname + "/views/" + "login.html");
+    if (login_msg === '') {
+        login_msg = 'welcome login'
+        res.render("login", {
+            msg: login_msg
+        });
+    } else {
+        login_msg = 'welcome login'
+        res.render("login", {
+            msg: login_msg
+        });
+    }
 })
 router.get('/login', function(req, res) {
-    res.sendFile(__dirname + "/views/" + "login.html");
+    if (login_msg === 'register success') {
+        res.render("login", {
+            msg: login_msg
+        });
+    } else if (login_msg === 'have no this user') {
+        res.render("login", {
+            msg: login_msg
+        });
+    } else if (login_msg === 'password not right') {
+        res.render("login", {
+            msg: login_msg
+        });
+    } else {
+        login_msg = 'welcome login'
+        res.render("login", {
+            msg: login_msg
+        });
+    }
+
 })
 
 //注销
@@ -47,13 +88,10 @@ router.get('/home', function(req, res) {
 
 })
 
-//
-
 //表单提交数据处理
 
 //登陆
 router.post('/login_post', urlencodedParser, function(req, res) {
-
     var name = req.body.name;
     var passwd = req.body.passwd;
     var dirAcc = JSON.parse(localStorage.getItem('users'));
@@ -65,6 +103,7 @@ router.post('/login_post', urlencodedParser, function(req, res) {
             }
         }
         if (id === -1) { //无此用户
+            login_msg = 'have no this user'
             res.redirect('/login');
             console.log('have no this user:' + name);
         } else if (passwd === dirAcc[id].passwd) { //密码正确 存储session
@@ -76,10 +115,12 @@ router.post('/login_post', urlencodedParser, function(req, res) {
             res.redirect('/home');
             console.log(name + ' login success');
         } else {
+            login_msg = 'password not right';
             res.redirect('/login');
             console.log('password not right');
         }
     } else { //无用户库 注册
+        register_msg = '';
         res.redirect('/register');
         console.log('have no user,register');
     }
@@ -103,6 +144,7 @@ router.post('/register_post', urlencodedParser, function(req, res) {
             }
             if (existSate) { //用户存在
                 res.redirect('/register');
+                register_msg = 'user already exist';
                 console.log('user already exist');
             } else { //用户不存在 创建
                 let user = {
@@ -111,6 +153,7 @@ router.post('/register_post', urlencodedParser, function(req, res) {
                 };
                 dirAcc.push(user);
                 localStorage.setItem('users', JSON.stringify(dirAcc));
+                login_msg = 'register success';
                 res.redirect('/login');
                 console.log(name + ' register success');
             }
@@ -120,10 +163,12 @@ router.post('/register_post', urlencodedParser, function(req, res) {
                 passwd: passwd1
             }]
             localStorage.setItem('users', JSON.stringify(user));
+            login_msg = 'register success';
             res.redirect('/login');
             console.log(name + ' register success');
         }
     } else {
+        register_msg = 'passwd not same';
         res.redirect('/register');
         console.log('passwd not same');
     }
